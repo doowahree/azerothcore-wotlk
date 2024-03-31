ac6d75bdb635dbad776cb2b2716114b70e7b6a80
doowahree <33791774+doowahree@users.noreply.github.com>
2024-03-30 18:49:11 -0700

    fix(db/creature): Set faction of low level frostmane trolls to troll, frostmane.
    fix: The low level Frostmane Novice, Frostmane Troll Whelp, and the elite Grik'nir the Cold to Frostmane faction and properly be hostile to alliance.

--- data/sql/updates/pending_db_world/frostmane_faction_fix.sql
+++ data/sql/updates/pending_db_world/frostmane_faction_fix.sql
@@ -1,2 +1,2 @@
 -- All creature with name starting in "Frostmane" should be set to faction 37, "Troll, Frostmane".
-UPDATE creature_template SET faction = 37 WHERE name LIKE "Frostmane%";
+UPDATE creature_template SET faction = 37 WHERE name LIKE "Grik'nir%" OR name LIKE "Frostmane%";
